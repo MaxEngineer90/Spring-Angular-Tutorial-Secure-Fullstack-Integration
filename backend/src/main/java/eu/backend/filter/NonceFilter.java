c7816417a8f300f26e71d5dbae54bda635567800
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-10-29 21:07:25 +0100

    add nonce

--- backend/src/main/java/eu/backend/filter/NonceFilter.java
+++ backend/src/main/java/eu/backend/filter/NonceFilter.java
@@ -8,8 +8,6 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -21,7 +19,7 @@ import java.util.Base64;
 public class NonceFilter implements Filter {
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) {
         // Initialization logic if needed
         log.info("NonceFilter initialized");
     }
